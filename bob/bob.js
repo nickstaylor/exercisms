2e199383d1f3858b65ee21c4a29b8560ad4e494a
Nick Taylor <nickstaylor@gmail.com>
2020-07-17 21:09:59 -0600

    Bob exercism complete

--- bob/bob.js
+++ bob/bob.js
@@ -1,19 +1,41 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
 export const hey = (message) => {
-  let capsMessage = message.toUpperCase()
+  message = message.trim()
+  message.replace('\t', '')
+  .replace('\n', '')
+  .replace('\r', '')
   let splitMessage = message.split('')
-  if (message === capsMessage && splitMessage[splitMessage.length-1] !== '?'){
+  let capsMessage = message.toUpperCase()
+
+  //fine, be that way
+  if(!message || !splitMessage.length){
+    return 'Fine. Be that way!'
+  }
+  //whoa, chill out
+  if (message === capsMessage &&
+    (capsMessage.match(/[A-Z]/g)) &&
+    (splitMessage[splitMessage.length-1] === '!'
+    || splitMessage[splitMessage.length -1] !== '?')){
     return 'Whoa, chill out!'
   }
-  if (message === capsMessage && splitMessage[splitMessage.length-1] === '?' && capsMessage.match(/[A-Z]/g)){
+
+  //calm down I know what I'm doing
+  if (message === capsMessage &&
+     splitMessage[splitMessage.length-1] === '?' &&
+      capsMessage.match(/[A-Z]/g)){
     return 'Calm down, I know what I\'m doing!'
   }
-  // if (!capsMessage.match(/[A-Z]/g) && splitMessage[splitMessage.length-1] === '?'){
-  //   return 'Sure.'
-  // }
+
+  if (message !== capsMessage &&
+  splitMessage[splitMessage.length-1] === '?'){
+    return 'Sure.'
+  }
+
+  if (!capsMessage.match(/[A-Z]/g) &&
+  splitMessage[splitMessage.length-1] === '?'){
+    return 'Sure.'
+  } else {
+    return "Whatever."
+  }
 
 };
