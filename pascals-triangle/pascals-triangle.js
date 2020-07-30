fdd1e2dd07fbab1b5cbb2cb82b76fb31f1b523b7
Nick Taylor <nickstaylor@gmail.com>
2020-07-29 21:22:33 -0600

    Solved pascal's triangle!

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,30 @@
+import { loadPartialConfig } from "@babel/core";
+
+
+export const rows = (number) => {
+  if (!number){return []}
+  if (number === 1){return [[1]]}
+  let newArray = [[1], [1, 1]]
+  if (number ===2){return newArray}
+  //start at array 2 and run until number length
+  for (let i = 2; i < number; i++){
+    let tempArray = [1]
+    for (let j = 1; j <= i; j++){
+      let pushedNumber;
+      if (j === i){
+        pushedNumber = 1
+      }
+      else {
+        //this needs to be the previous array
+        pushedNumber = newArray[newArray.length-1][j-1] + newArray[newArray.length-1][j]
+      }
+      tempArray.push(pushedNumber)
+    }
+    newArray.push(tempArray)
+  }
+
+  console.log('newArray', newArray);
+
+  return newArray
+
+};
