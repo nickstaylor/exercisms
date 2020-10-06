0ed000c5651040a7ee3e3235312b532f63f416ac
Nick Taylor <nickstaylor@gmail.com>
2020-10-06 11:19:01 -0600

    First five tests passing

---
+++ robot-name/robot-name.js
@@ -0,0 +1,52 @@
+// This is only a SKELETON file for the 'Robot Name' exercise. It's been
+// provided as a convenience to get your started writing code faster.
+
+export class Robot {
+    constructor() {
+        this.allNames = [];
+        this.name = this.getNewName();
+    }
+
+    
+    makeRandomName() {
+        let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let r1 = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+        let r2 = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+        let numbers = '0123456789';
+        let n1 = numbers.charAt(Math.floor(Math.random() * numbers.length))
+        let n2 = numbers.charAt(Math.floor(Math.random() * numbers.length))
+        let n3 = numbers.charAt(Math.floor(Math.random() * numbers.length))
+        return [r1, r2, n1, n2, n3].join("");
+    }
+
+    getNewName() {
+        let name = this.makeRandomName()
+        if (this.allNames.includes(name)) {
+          return this.getNewName()
+        } else {
+          this.allNames.push(name);
+            return name
+        }
+    }
+
+    
+    reset() {
+        this.name = this.getNewName()
+    }
+
+}
+ 
+export class Set {
+    constructor() {
+        this.names = [];
+        this.size = this.names.length
+    }
+
+    add(name) {
+        this.names.push(name)
+    }
+
+
+}
+
+Robot.releaseNames = () => { };
